7c1dbb0f91da71dd8bac5c4d85386828ebffc6d5
Tema2135 <temaperne@gmail.com>
2024-09-13 11:31:35 +0500

    TT

---
+++ src/test/java/org/example/javaeelaboratory/JavaeelaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaeelaboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaeelaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
